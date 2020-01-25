18e24dbf1ea911f4f980f8c8942b9fea0b5efb6c
Dimitri <dimitri.brukakis@cubic.com>
2020-01-25 17:31:22 +0100

    Initial Commit

---
+++ Infogate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Infogate
+//
+//  Created by Brukakis, Dimitrios (DE) on 25.01.20.
+//  Copyright © 2020 Brukakis, Dimitrios (DE). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
